386c3d7756ff7955442c7bbccd842c1c17b3fddf
Demecio Sanabria <demesiosanabri@gmail.com>
2024-04-06 17:20:02 -0700

    Updated python files to PEP8

--- exercise_2.py
+++ exercise_2.py
@@ -5,55 +5,56 @@
 
 import tifffile
 import numpy as np
-from pymmcore_plus import CMMCorePlus, DeviceAdapter #device adapter allows me to change camera mode
+from pymmcore_plus import CMMCorePlus
 from useq import MDASequence, MDAEvent
 
 # open instance with demo config as in exercise 1
 mmc = CMMCorePlus.instance()
 mmc.loadSystemConfiguration(r"C:\Program Files\Micro-Manager-2.0\MMConfig_demo.cfg")
-mmc.setProperty("Camera", "Mode", "Noise") #changes camera mode to noise
+mmc.setProperty("Camera", "Mode", "Noise")  # changes camera mode to noise
 
-#defining attributes for this acquisition
-ch = [{"config" :"DAPI", "exposure": 10}] #10 ms exposure
-timePlan = {"interval": 0, "loops": 100} # 100 time points
+# defining attributes for this acquisition
+ch = [{"config": "DAPI", "exposure": 10}]  # 10 ms exposure
+timePlan = {"interval": 0, "loops": 100}  # 100 time points
 
-#pixel value target and radius to set to zero
-target = 700 
+# pixel value target and radius to set to zero
+target = 700
 radius = 15
 
-#def multi-dimensional acquisition
-seq = MDASequence(channels=ch, time_plan=timePlan) 
+# def multi-dimensional acquisition
+seq = MDASequence(channels=ch, time_plan=timePlan)
 
-#checks if pixel at (x, y) is located within radius centered at coords and sets to 0
-def check_radius(data: np.ndarray, coords: list): 
+# checks if pixel at (x, y) is located within radius centered at coords and sets to 0
+def check_radius(data: np.ndarray, coords: list):
     if len(coords) == 0:
         return data
     for coord in coords:
         for x in range(len(data)):
             for y in range(len(data[0])):
-                rad_sq = (x-coord[0])**2 + (y-coord[1])**2 #circle centered at coords
+                rad_sq = (x-coord[0])**2 + (y-coord[1])**2  # circle centered at coords
                 if rad_sq <= (radius**2):
-                    data[x,y] = 0
+                    data[x, y] = 0
                 else:
                     pass
     return data
 
-#edits every image captured during acquisition process. Modified from on_image_captured function at: https://pymmcore-plus.github.io/pymmcore-plus/guides/events/
+# edits every image captured during acquisition process. Modified from 
+# on_image_captured function at: https://pymmcore-plus.github.io/pymmcore-plus/guides/events/
 @mmc.mda.events.frameReady.connect
 def on_image_captured(data: np.ndarray, event: MDAEvent): 
-    coords_spec = [] # all coords which have pixel valuye of 700 go here
+    coords_spec = []  # all coords which have pixel valuye of 700 go here
     print(f"Event index {event.index} captured with shape {data.shape}")
 
     # first check for all pixels with value = 700 and save coordinates
     for x in range(len(data)):
-        for y in range(len(data[0])): 
-            if data[x,y] == target:
-                data[x,y] = 0
-                coords_spec.append([x,y])
-    #then begin checking for all pixels within radius of 15 centered at coords_spec
+        for y in range(len(data[0])):
+            if data[x ,y] == target:
+                data[x ,y] = 0
+                coords_spec.append([x ,y])
+    # then begin checking for all pixels within radius of 15 centered at coords_spec
     data = check_radius(data, coords_spec)
     if len(coords_spec) != 0:
         print("700 pixel value at:", coords_spec)
 
-run = mmc.run_mda(seq, output = r"exercise_2_data.tiff") 
 
+run = mmc.run_mda(seq, output=r"exercise_2_data.tiff") 
--- exercise_3.py
+++ exercise_3.py
@@ -1,29 +1,32 @@
-# Measure the eccentricity of each nucleus you detected and plot the results as a histogram. 
-# Label the histogram such that we can understand the data it shows.
+# Measure the eccentricity of each nucleus you detected and plot the results 
+# as a histogram. Label the histogram such that we can understand the data it shows.
 
-import matplotlib.pyplot as plt #tool for plotting
+import matplotlib.pyplot as plt  # tool for plotting
 import tifffile as tiff
 import numpy as np
-import skimage as ski #tool for image analysis, here used for segmentation and finding eccentricities 
-from scipy import ndimage #tool used for image analysis, here used for segmentation
-from matplotlib import rc #formatting figures
-rc('font',**{'size': 16})
-rc('axes', **{'labelsize':16, 'linewidth':1})
-rc('xtick', **{'direction':'in', 'top':True, 'minor.visible':True})
-rc('ytick', **{'direction':'in', 'right':True, 'minor.visible':True})
+import skimage as ski  # tool for image analysis, here used for segmentation and finding eccentricities 
+from scipy import ndimage  # tool used for image analysis, here used for segmentation
+from matplotlib import rc  # formatting figures
+rc('font', **{'size': 16})
+rc('axes', **{'labelsize': 16, 'linewidth': 1})
+rc('xtick', **{'direction': 'in', 'top': True, 'minor.visible': True})
+rc('ytick', **{'direction': 'in', 'right': True, 'minor.visible': True})
+
 
 def eccentricity(maj_axis, min_axis):
     return np.sqrt(abs(maj_axis**2 - min_axis**2)) / maj_axis
+
 
 with tiff.TiffFile('Assoc_RDEng_test.tif') as images:
     for iter, page in enumerate(images.pages, 1):
         image = page.asarray()
 
-        #creating threshold for region based segmentation similar to: https://scikit-image.org/docs/stable/user_guide/tutorial_segmentation.html#region-based-segmentation
+        # creating threshold for region based segmentation similar to: 
+        # https://scikit-image.org/docs/stable/user_guide/tutorial_segmentation.html#region-based-segmentation
         std_dev = image.std()
         min = image.min()
 
-        #looking for edges of nuclei
+        # looking for edges of nuclei
         edges = np.zeros_like(image)
         edges[image <= (min + std_dev)] = 1
         edges[image > (min + std_dev)] = 2
@@ -31,22 +34,22 @@ with tiff.TiffFile('Assoc_RDEng_test.tif') as images:
 
         segment = ski.segmentation.watershed(elevation_map, edges)
 
-        #removing small holes and getting edges of segmented image
+        # removing small holes and getting edges of segmented image
         segment = ndimage.binary_fill_holes(segment-1) 
         nuclei_edges = ski.feature.canny(segment)
 
-        #labeling and locating all nuclei 
+        # labeling and locating all nuclei 
         labels, num_features = ndimage.label(segment)
         nuclei = ndimage.find_objects(labels)
 
         print(f"Succesfully segmented page {iter}!\n\n", 
               "Beginning eccentricity measurements")
 
-        e = []#initialize list for eccentricty values
+        e = []  # initialize list for eccentricty values
 
         # will iterate over all found objects (nuclei) 
         for obj, nucleus in enumerate(nuclei, 1):
-            #detect ellipsis and pick one with highest accumulator, nonzero major axes
+            # detect ellipsis and pick one with highest accumulator, nonzero major axes
             ellipse = ski.transform.hough_ellipse(nuclei_edges[nucleus], accuracy=11)
             if len(ellipse) == 0: 
                 continue
@@ -66,7 +69,7 @@ with tiff.TiffFile('Assoc_RDEng_test.tif') as images:
                     continue
         
         # makes histograms for each page
-        plt.hist(e, bins = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1])
+        plt.hist(e, bins=[0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1])
         plt.xlabel('Eccentricity')
         plt.ylabel('Nuclei count')
         plt.title(f"Eccentriciities of nuclei in page {iter}")
